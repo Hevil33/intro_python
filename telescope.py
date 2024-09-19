46021e41541fb67c684ac1ff726f2687a7dcb8ae
herve.haudemand <herve.haudemand@inaf.it>
2024-09-19 14:52:14 +0200

    first commit

---
+++ telescope.py
@@ -0,0 +1,36 @@
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# define a class named telescope
+class telescope:
+    # initialization
+    def __init__(self, resolution):
+        self.resolution = resolution  # ATTRIBUTE
+
+    # METHOD
+    def observe(self):
+        image = np.empty(shape=(self.resolution, self.resolution))
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                image[i, j] = random.random()
+
+        return image
+
+
+# create two objects of type telescope with different resolution
+telescope1 = telescope(100)
+telescope2 = telescope(resolution=200)
+
+# call observe on each object to get two images
+image1 = telescope1.observe()
+image2 = telescope2.observe()
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(image1)
+ax[0].set_title("Telescope 1")
+ax[1].imshow(image2)
+ax[1].set_title("Telescope 2")
+plt.show()
